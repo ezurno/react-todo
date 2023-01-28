436dc83e6e32d2cca1c9d2ab2af477dad825b096
ezurno <ezurno@gmail.com>
2023-01-28 19:40:17 +0900

    LEARN: set custom validation
    1. 유효성을 직접 부여 할 수 있음
    
    2. 유효성 검사 함수에 async를 걸어서 서버와 비동기로 값을 주고받을 수 있음 ex) 이름이 같은 사람이 있는지

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -8,6 +8,7 @@ interface IFormData {
   userName: string;
   password: string;
   passwordCheck: string;
+  extraError?: string;
 } //React Hook Form은 TypeScript로 빌드되었으며, FormData 유형을 정의하여 form 값을 지원할 수 있다
 
 function ToDoList() {
@@ -18,6 +19,7 @@ function ToDoList() {
     register,
     /*watch,*/ handleSubmit,
     formState: { errors },
+    setError,
   } = useForm<IFormData>({
     defaultValues: {
       email: "@gmail.com", // 초기값도 정해 줄 수 있음
@@ -31,19 +33,25 @@ function ToDoList() {
   /**
    * 유효성 검사(handelSubmit) 후 만족 시 실행
    */
-  const onValid = (data: any) => {
-    console.log(data);
+  const onValid = (data: IFormData) => {
+    if (data.password !== data.passwordCheck) {
+      setError(
+        "passwordCheck",
+        { message: "password is not same." },
+        { shouldFocus: true } // error 발생시 cursor 이동
+      );
+    }
+
+    // setError("extraError", { message: "Server downed." });
   };
 
-  const inValid = () => {
-    console.log(errors);
-  };
+  console.log(errors);
 
   return (
     <div>
       <form
         style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid, inValid)}
+        onSubmit={handleSubmit(onValid)}
       >
         {/* {...}로 풀어쓰기 하여 props 등록 */}
         <input
@@ -51,15 +59,33 @@ function ToDoList() {
             required: "E-mail is required",
 
             pattern: {
-              value: /^[A-Za-z0-9]+@+[A-Za-z0-9]+\.+[A-Za-z0-9]$/,
+              value: /^[A-Za-z0-9]+@+[A-Za-z0-9]+\.+[A-Za-z0-9]/,
               message: "plz input email form",
             },
           })}
           placeholder="email"
         />
         <span>{errors?.email?.message}</span>
-        <input {...register("firstName")} placeholder="first name" />
-        <input {...register("lastName")} placeholder="last name" />
+        <input
+          {...register("firstName", {
+            validate: (value) =>
+              value.includes("lee") ? "illigal name" : true,
+          })} // 유효성 직접 부여 하기
+          placeholder="first name"
+        />
+        <span>{errors?.firstName?.message}</span>
+
+        <input
+          {...register("lastName", {
+            validate: {
+              noLee: (value) => (value.includes("lee") ? "illigal name" : true),
+              noJ: (value) => (value.includes("J") ? "illigal name" : true), // 직접 validate 함수 구현 가능
+            },
+          })}
+          placeholder="last name"
+        />
+        <span>{errors?.lastName?.message}</span>
+
         <input {...register("userName")} placeholder="user name" />
         <input
           {...register("password", {
@@ -71,7 +97,10 @@ function ToDoList() {
           })}
           placeholder="password"
         />
+        <span>{errors?.password?.message}</span>
+
         <input {...register("passwordCheck")} placeholder="password check" />
+        <span>{errors?.passwordCheck?.message}</span>
 
         <button>submit</button>
       </form>
