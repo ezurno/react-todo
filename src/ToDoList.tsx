5f573101b8148f1d2213eb7e9ad095077bb4ff32
ezurno <ezurno@gmail.com>
2023-01-28 18:29:24 +0900

    LEARN: use react-hook-form
    1. npm i react-hook-form
    
    2.
     - register => 등록
     - watch => 구독
     -  handleSubmit => 유효성 검사
     - formState => .errors로 error 체크
    
    3. F12로 데이터 폼을 보면서 입력하는걸 습관화 하기

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,25 +1,51 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
+  /**
+   * register로 등록, watch로 구독, handleSubmit으로 요구조건 충족하는지 체크
+   */
+  const { register, /*watch,*/ handleSubmit, formState } = useForm();
+  /*
+    console.log(watch());
+    => watch 함수로 구독한 정보 가져올 수 있음
+*/
 
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
+  /**
+   * 유효성 검사(handelSubmit) 후 만족 시 실행
+   */
+  const onValid = (data: any) => {
+    console.log(data);
   };
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  console.log(formState.errors);
 
   return (
-    <form onSubmit={onSubmit}>
-      <input onChange={onChange} placeholder="Write to do list here." />
-      <button>submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit(onValid)}>
+        {/* {...}로 풀어쓰기 하여 props 등록 */}
+        <input
+          {...register("Email", { required: true, minLength: 5 })}
+          placeholder="e-mail"
+        />
+        <input {...register("firstName")} placeholder="first name" />
+        <input {...register("lastName")} placeholder="last name" />
+        <input {...register("userName")} placeholder="user name" />
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short.",
+            },
+          })}
+          placeholder="password"
+        />
+        <input {...register("passwordCheck")} placeholder="password check" />
+
+        <button>submit</button>
+      </form>
+    </div>
   );
 }
 
