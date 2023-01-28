75fe7587632d1e758bfa1d731813a40c4bf953c0
ezurno <ezurno@gmail.com>
2023-01-28 19:11:32 +0900

    LEARN: errors check validation
    1. useForm에는 generic으로 type을 지정 해주는 편이 좋음
    
    2. pattern 으로 입력 폼의 값을 정의 할 수 있음

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,11 +1,28 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
+interface IFormData {
+  email: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  password: string;
+  passwordCheck: string;
+} //React Hook Form은 TypeScript로 빌드되었으며, FormData 유형을 정의하여 form 값을 지원할 수 있다
+
 function ToDoList() {
   /**
    * register로 등록, watch로 구독, handleSubmit으로 요구조건 충족하는지 체크
    */
-  const { register, /*watch,*/ handleSubmit, formState } = useForm();
+  const {
+    register,
+    /*watch,*/ handleSubmit,
+    formState: { errors },
+  } = useForm<IFormData>({
+    defaultValues: {
+      email: "@gmail.com", // 초기값도 정해 줄 수 있음
+    },
+  });
   /*
     console.log(watch());
     => watch 함수로 구독한 정보 가져올 수 있음
@@ -18,16 +35,29 @@ function ToDoList() {
     console.log(data);
   };
 
-  console.log(formState.errors);
+  const inValid = () => {
+    console.log(errors);
+  };
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onValid)}>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid, inValid)}
+      >
         {/* {...}로 풀어쓰기 하여 props 등록 */}
         <input
-          {...register("Email", { required: true, minLength: 5 })}
-          placeholder="e-mail"
+          {...register("email", {
+            required: "E-mail is required",
+
+            pattern: {
+              value: /^[A-Za-z0-9]+@+[A-Za-z0-9]+\.+[A-Za-z0-9]$/,
+              message: "plz input email form",
+            },
+          })}
+          placeholder="email"
         />
+        <span>{errors?.email?.message}</span>
         <input {...register("firstName")} placeholder="first name" />
         <input {...register("lastName")} placeholder="last name" />
         <input {...register("userName")} placeholder="user name" />
