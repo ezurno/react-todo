a24e5b771cfed2dfbe3bceb1b3c84d04f20a96fc
ezurno <ezurno@gmail.com>
2023-01-29 18:00:10 +0900

    LEARN: use slice to Immutation
    1. immutation을 해주기 위해 newToDo를 slice로 잘라서 새롭게 만들어 반환
    
    2. newToDo의 interface를 직접적으로 category가 oldToDos의 category와 type이 같다는걸 직접적으로 수정

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,4 +1,4 @@
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../Atoms";
 
 function ToDo({ id, category, text }: IToDo) {
@@ -9,9 +9,14 @@ function ToDo({ id, category, text }: IToDo) {
     } = event;
     setToDos((oldToDos) => {
       const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-      const newToDo = { text, id, category: name };
+      const newToDo: IToDo = { text, id, category: name as IToDo["category"] };
+      // 새롭게 type을 지정해주어야 함 안그러면 oldToDos에서 받는 type이 달라서 error
       console.log(targetIndex);
-      return oldToDos;
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ]; // 값을 slice로 잘라서 새로 만들어 반환
     });
   };
 
