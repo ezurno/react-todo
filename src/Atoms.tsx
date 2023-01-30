f111a5b394950af69a17100db1d3b7c56f357602
ezurno <ezurno@gmail.com>
2023-01-30 15:43:37 +0900

    LEARN: selector
    1. selector를 사용해 atom 에서 값을 직접적으로 수정을 하지 않고 새 배열을 만듦.
    
    2. 수정은 하지 않지만 의존적인 동적함수

--- src/Atoms.tsx
+++ src/Atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   // 그냥 쓰면 toDoState의 default가 type이 정의가 안되어서 never type이기 때문에 type을 정해주기 위해 사용
@@ -14,4 +14,20 @@ export interface IForm {
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+/**
+ * selector로 toDoState를 가져와 새 배열을 만들지만 수정은 하지 않는 의존적이지만 동적인 함수
+ */
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    // get은 object의 .get 함수
+    const toDos = get(toDoState);
+    return [
+      toDos.filter((toDo) => toDo.category === "TODO"),
+      toDos.filter((toDo) => toDo.category === "DOING"),
+      toDos.filter((toDo) => toDo.category === "DONE"),
+    ];
+  },
 });
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { useRecoilValue } from "recoil";
-import { toDoState } from "../Atoms";
+import { toDoSelector, toDoState } from "../Atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
+  // const toDos = useRecoilValue(toDoState);
+  const [toDos, Doing, Done] = useRecoilValue(toDoSelector); // Selector가 [{}, {}, {}]형태
 
   console.log(toDos);
 
@@ -13,8 +14,27 @@ function ToDoList() {
     <div>
       <h1>To Do List</h1>
       <CreateToDo />
+
+      <h2>toDos</h2>
+      <hr />
       <ul>
         {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+
+      <h2>Doing</h2>
+      <hr />
+      <ul>
+        {Doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+
+      <h2>Done</h2>
+      <hr />
+      <ul>
+        {Done.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
