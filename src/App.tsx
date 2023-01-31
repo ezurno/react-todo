c10a4bdca9f9c6e991d6d0b95bba5302bef03f50
ezurno <ezurno@gmail.com>
2023-01-31 21:52:43 +0900

    SET: dnd environment
    1.
     - npm i react-beautiful-dnd
     - npm i @types/react-beautiful-react-dnd --legacy-peer-deps
    
    2. https://github.com/LeeHyungGeun/react-beautiful-dnd-kr

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
-import { useRecoilState, useRecoilValue } from "recoil";
-import { hourSelector, minuteState } from "./Atoms";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSelector);
-
-  const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);
-    // input의 값은 기본적으로 string로 들어오기 때문에 +를 사용해 하나만 들어오게 함
-  };
-
-  const onHourChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value);
-  };
-
-  return (
-    <div>
-      <input
-        value={minutes}
-        onChange={onMinuteChange}
-        type="number"
-        placeholder="Minutes"
-      />
-      <input
-        value={hours}
-        onChange={onHourChange}
-        type="number"
-        placeholder="Hours"
-      />
-    </div>
-  );
+  return null;
 }
 
 export default App;
