8da42c98bb81c719d5176976827542f79f9637e0
ezurno <ezurno@gmail.com>
2023-02-02 22:29:43 +0900

    LEARN: React.memo
    1. React는 parent node가 수정이 되면 children node가 전부 수정 됨, react의 장점이지만 여기선 단점 (dnd 시 너무 많은 rerendering)
    
    2. React.memo를 사용하면 react에게 이 component는 props가 수정이 되지 않으면 rerendering 하지말라고 알려줌

--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
@@ -29,4 +30,4 @@ function DraggableCard({ toDo, index }: IDraggableCardProps) {
   );
 }
 
-export default DraggableCard;
+export default React.memo(DraggableCard);
