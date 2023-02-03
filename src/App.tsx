a4647b4090e685554ac86146b1e2c14ba0b04b86
ezurno <ezurno@gmail.com>
2023-02-03 18:37:10 +0900

    MOD: modified default value
    1. board를 3개를 만들기 위해 default value를 object 식으로 바꿈
    
    2. toDos의 default 값을 [] 로 찾을 수 있음 ex) toDos[boardId]
    
    3. 새 board를 추가 할 수 있으므로 [key: string]: string[]; 으로 interface를 만들어 줌

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,11 @@ import {
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./Atoms";
+import Board from "./components/Board";
 import DraggableCard from "./components/DraggableCard";
 const Wrapper = styled.div`
   display: flex;
-  max-width: 480px;
+  max-width: 960px;
   width: 100%;
   margin: 0 auto;
   justify-content: center;
@@ -20,14 +21,8 @@ const Wrapper = styled.div`
 const Boards = styled.div`
   display: grid;
   width: 100%;
-  grid-template-columns: repeat(1, 1fr);
-`;
-const Board = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 200px;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 15px;
 `;
 
 function App() {
@@ -35,35 +30,28 @@ function App() {
   const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
     if (destination?.index === undefined) return; // 목적지가 없으면 실행 x
 
-    setToDos((oldToDos) => {
-      const toDosCopy = [...oldToDos];
-      // 1) Delete item on source.index
-      console.log("Delete item on", source.index);
-      console.log(toDosCopy);
-      toDosCopy.splice(source.index, 1); // splice로 index의 위치에서 1개의 index를 추출
-      console.log("Deleted item");
-      console.log(toDosCopy);
-      // 2) Put back the item on the destination.index
-      console.log("Put back", draggableId, "on ", destination.index);
-      toDosCopy.splice(destination.index, 0, draggableId); // splice로 index의 위치에서 수정하지 않고 draggableId를 주입
-      console.log(toDosCopy);
-      return toDosCopy;
-    });
+    // setToDos((oldToDos) => {
+    //   const toDosCopy = [...oldToDos];
+    //   // 1) Delete item on source.index
+    //   console.log("Delete item on", source.index);
+    //   console.log(toDosCopy);
+    //   toDosCopy.splice(source.index, 1); // splice로 index의 위치에서 1개의 index를 추출
+    //   console.log("Deleted item");
+    //   console.log(toDosCopy);
+    //   // 2) Put back the item on the destination.index
+    //   console.log("Put back", draggableId, "on ", destination.index);
+    //   toDosCopy.splice(destination.index, 0, draggableId); // splice로 index의 위치에서 수정하지 않고 draggableId를 주입
+    //   console.log(toDosCopy);
+    //   return toDosCopy;
+    // });
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-          <Droppable droppableId="one">
-            {(magic) => (
-              <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <DraggableCard toDo={toDo} index={index} key={toDo} />
-                ))}
-                {magic.placeholder}
-              </Board>
-            )}
-          </Droppable>
+          {Object.keys(toDos).map((boardId) => (
+            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
+          ))}
         </Boards>
       </Wrapper>
     </DragDropContext>
